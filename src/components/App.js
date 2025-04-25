c41fe3399009f2b24149c3bb2050d469651f4a5c
Patrick <patrick.andre@student.moringaschool.com>
2025-04-25 21:45:36 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,37 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const deleteTask = (indexToDelete) => {
+    setTasks(tasks.filter((_, index) => index !== indexToDelete));
+  };
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleFilter(){
+    if(selectedCategory === "All")return tasks;
+    else{
+      return tasks.filter((task) => task.category === selectedCategory)
+    }
+  }
+  const onTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+      categories={CATEGORIES}
+      selectedCategory ={selectedCategory}
+      onCategorySelect = {setSelectedCategory}
+       />
+       
+      <NewTaskForm 
+      categories={CATEGORIES}
+      onTaskFormSubmit ={onTaskFormSubmit}/>
+      <TaskList 
+      tasks={handleFilter()} 
+      onDelete={deleteTask}/>
     </div>
   );
 }
